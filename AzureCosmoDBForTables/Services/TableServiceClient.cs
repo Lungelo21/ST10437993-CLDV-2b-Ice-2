b8a034419f7e185859e72c8cd8b22a179b49d392
ST10437993 <166373795+Lungelo21@users.noreply.github.com>
2025-10-03 17:48:26 +0200

    commit

---
+++ AzureCosmoDBForTables/Services/TableServiceClient.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Data.Tables;
+using Azure.Identity;
+
+namespace AzureCosmoDBForTables.Services
+{
+    public class TableServiceClient : TableClient
+    {
+        DefaultAzureCredential credential = new();
+
+        TableServiceClient serviceClient = new(
+            endpoint: new Uri("<azure-cosmos-db-table-account-endpoint>"),
+            credential
+        );
+
+        public async Task<string> GetTableClient(string tableName)
+        {
+            TableClient client = serviceClient.GetTableClient(
+                        tableName: "<azure-cosmos-db-table-name>"
+                    );
+        }
+    }
+}
